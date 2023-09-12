2fd1b431a994af7836d2609acd5d78ad32542ad8
Onur Öner <onuroner4054@gmail.com>
2023-09-12 23:15:41 +0300

    Inıtial commit

---
+++ ClassInstanceFieldProperty/ClassInstanceFieldProperty/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInstanceFieldProperty
+{
+    public class Employee
+    {
+        public string Name;
+        public string Surname;
+        public int No;
+        public string Department;
+
+        public void EmployeeInfo()
+        {
+            Console.WriteLine($"Çalışanın Adı: {Name}");
+            Console.WriteLine($"Çalışanın Soyadı: {Surname}");
+            Console.WriteLine($"Çalışanın Numarası: {No}");
+            Console.WriteLine($"Çalışanın Departmanı: {Department}");
+        }
+    }
+}
---
+++ ClassInstanceFieldProperty/ClassInstanceFieldProperty/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ClassInstanceFieldProperty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            employee.Name = "Onur";
+            employee.Surname = "Öner";
+            employee.No = 123456;
+            employee.Department = "İnsan Kaynakları";
+
+            employee.EmployeeInfo();
+
+        }
+    }
+}
