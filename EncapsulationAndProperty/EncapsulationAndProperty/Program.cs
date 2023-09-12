996e8e527bc0535253bd8b2f9399a4b0e45e45dd
Onur Öner <onuroner4054@gmail.com>
2023-09-13 00:18:38 +0300

    Encapsulation homework

---
+++ EncapsulationAndProperty/EncapsulationAndProperty/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace EncapsulationAndProperty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Name = "Onur";
+            student.Surname = "Öner";
+            student.StudentNo = 12345;
+            student.Classroom = 3;
+            student.GetStudentInfo();
+            student.IncreaseClassroom();
+            student.GetStudentInfo();
+
+            Student student2 = new Student("Akın","Öner",54321,1);
+            student2.DecreaseClassroom();
+            student2.DecreaseClassroom();
+            student2.GetStudentInfo();
+        }
+    }
+}
---
+++ EncapsulationAndProperty/EncapsulationAndProperty/Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationAndProperty
+{
+    public class Student
+    {
+        private string name;
+        private string surname;
+        private int studentNo;
+        private int classroom;
+
+        
+
+        public string Name { get => name; set => name = value; }
+        public string Surname { get => surname; set => surname = value; }
+        public int StudentNo { get => studentNo; set => studentNo = value; }
+        public int Classroom { 
+            get => classroom;
+            set { 
+                if(value < 1)
+                {
+                    Console.WriteLine("Sınıf 1'den küçük olamaz.");
+                    classroom = 1;
+                }
+                else
+                {
+                    classroom = value;
+                }
+            } 
+        }
+
+        public Student(string name, string surname, int studentNo, int classroom)
+        {
+            Name = name;
+            Surname = surname;
+            StudentNo = studentNo;
+            Classroom = classroom;
+        }
+
+        public Student() { }
+
+        public void GetStudentInfo()
+        {
+            Console.WriteLine("*****Öğrenci Bilgileri*****");
+            Console.WriteLine($"Öğrencinin Adı: {Name}");
+            Console.WriteLine($"Öğrencinin Soyadı: {Surname}");
+            Console.WriteLine($"Öğrencinin Numarası: {StudentNo}");
+            Console.WriteLine($"Öğrencinin Sınıfı: {Classroom}");
+            Console.WriteLine("\n");
+        }
+        public void IncreaseClassroom()
+        {
+            this.Classroom += 1;
+        }
+
+        public void DecreaseClassroom()
+        {
+            this.Classroom -= 1;
+        }
+    }
+}
