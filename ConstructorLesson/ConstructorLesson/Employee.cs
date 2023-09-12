48821e783bb8ebc683842846a7a9dbd702c2cff2
Onur Öner <onuroner4054@gmail.com>
2023-09-12 23:43:12 +0300

    ConstructorLesson homework

---
+++ ConstructorLesson/ConstructorLesson/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorLesson
+{
+    public class Employee
+    {
+        public string Name;
+        public string Surname;
+        public int No;
+        public string Department;
+
+        public Employee() { }
+        public Employee(string name, string surname, int no, string Department)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.No = no;
+            this.Department = Department;
+        }
+        public Employee(string name, string surname)
+        {
+            this.Name = name;
+            this.Surname = surname;
+        }
+        public void EmployeeInfo()
+        {
+            Console.WriteLine($"Çalışanın Adı: {Name}");
+            Console.WriteLine($"Çalışanın Soyadı: {Surname}");
+            Console.WriteLine($"Çalışanın Numarası: {No}");
+            Console.WriteLine($"Çalışanın Departmanı: {Department}");
+        }
+    }
+}
---
+++ ConstructorLesson/ConstructorLesson/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConstructorLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            employee1.Name = "Onur";
+            employee1.Surname = "Öner";
+            employee1.No = 123456;
+            employee1.Department = "İnsan Kaynakları";
+            Console.WriteLine("*****Çalışan 1*****");
+            employee1.EmployeeInfo();
+            Console.WriteLine("\n");
+
+            Employee employee2 = new Employee("Akın","Öner",654321,"Arge");
+            Console.WriteLine("*****Çalışan 2*****");
+            employee2.EmployeeInfo();
+            Console.WriteLine("\n");
+
+            Employee employee3 = new Employee("Ahmet", "Yılmaz");
+            Console.WriteLine("*****Çalışan 3*****");
+            employee3.EmployeeInfo();
+            Console.WriteLine("\n");
+        }
+    }
+}
